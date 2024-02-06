fa5b84335d7c4bd31abaa8cf01ce0fb9cf61d5b2
Tushit Saxena <113344760+Tushit11@users.noreply.github.com>
2024-02-06 14:16:38 +0530

    Create app.py
    python code to remove background from image

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from rembg import remove
+import os  
+
+app = Flask(__name__)  
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def process_image(input_path, output_path):
+    with open(input_path, 'rb') as i:
+        with open(output_path, 'wb') as o: 
+            input_data = i.read()
+            output_data = remove(input_data)
+            o.write(output_data)
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'Welcome'
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file:
+        input_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'output.png')
+
+        file.save(input_path)
+
+        process_image(input_path, output_path)
+
+        return jsonify({'message': 'Image processed successfully', 'output_path': output_path})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
