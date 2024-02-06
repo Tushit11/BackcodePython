e5b3f10bd46baf5e7cb7de04d74fbeb642e872e8
Tushit Saxena <113344760+Tushit11@users.noreply.github.com>
2024-02-06 15:58:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,27 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import base64
 from rembg import remove
-import os  
 
-app = Flask(__name__)  
+app = Flask(__name__)
+CORS(app)
 
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def process_image(input_path, output_path):
-    with open(input_path, 'rb') as i:
-        with open(output_path, 'wb') as o: 
-            input_data = i.read()
-            output_data = remove(input_data)
-            o.write(output_data)
-
-@app.route('/', methods=['GET'])
-def welcome():
-    return 'Welcome'
-
-
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'})
-
-    if file:
-        input_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'output.png')
-
-        file.save(input_path)
-
-        process_image(input_path, output_path)
-
-        return jsonify({'message': 'Image processed successfully', 'output_path': output_path})
+@app.route('/remove_background', methods=['POST'])
+def remove_background():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'})
+    
+    image_file = request.files['image']
+    
+    if image_file.filename == '':
+        return jsonify({'error': 'No selected image'})
+    
+    try:
+        image_data = image_file.read()
+        image_data = base64.b64encode(remove(image_data)).decode('utf-8')
+        return jsonify({'result': image_data})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
